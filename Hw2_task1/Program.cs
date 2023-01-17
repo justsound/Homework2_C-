7abfe762c2e8e6fc3f2b4a0f6af6dbb5324ded9e
Just_sound <just_sound@mail.ru>
2023-01-17 22:28:02 +0300

    Добавил задание на определение значений четвертей

---
+++ Hw2_task1/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Console.WriteLine("Введите число x: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число y: ");
+int y = int.Parse(Console.ReadLine()!);
+if 
+    else Console.WriteLine(3);*/
+
+// 20. Задать номер четверти, показать диапазоны для возможных координат
---
+++ Hw2_task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер четверти от 1 до 4: ");
+
+int num = new Random().Next(1, 5);
+
+if (num == 1) Console.WriteLine($"Для {num}-й четверти координаты точки должны быть заданы в диапазоне x > 0, y > 0");
+else if (num == 2) Console.WriteLine($"Для {num}-й четверти координаты точки должны быть заданы в диапазоне x > 0, y < 0");
+else if (num == 3) Console.WriteLine($"Для {num}-й четверти координаты точки должны быть заданы в диапазоне x < 0, y < 0");
+else Console.WriteLine($"Для {num}-й четверти координаты точки должны быть заданы в диапазоне x < 0, y > 0");
+
+  
