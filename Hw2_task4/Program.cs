4ced15b8e247f93e48ed44b2edbffbc8c371d2d3
Just_sound <just_sound@mail.ru>
2023-01-18 19:37:59 +0300

    Добавил решение по нахождению расстояния

--- Hw2_task4/Program.cs
+++ Hw2_task4/Program.cs
@@ -1,29 +1,64 @@
-﻿Console.Clear();
-/*
-Console.WriteLine("Введите координату х первой точки: ");
-int x1 = int.Parse( Console.ReadLine());
-Console.WriteLine("Введите координату у первой точки: ");
-int y1 = int.Parse( Console.ReadLine());
-Console.WriteLine("Введите координату z первой точки: ");
-int z1 = int.Parse( Console.ReadLine());
-Console.WriteLine("Введите координату x второй точки: ");
-int x2 = int.Parse( Console.ReadLine());
-Console.WriteLine("Введите координату y второй точки: ");
-int y2 = int.Parse( Console.ReadLine());
-Console.WriteLine("Введите координату z второй точки: ");
-int z2 = int.Parse( Console.ReadLine());
-
-double distance = Math.Sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1) + (z2-z1) * (z2-z1));
-
-Console.WriteLine("Расстояние между точками в 3D пространстве = " + distance); */
-
-void FillArray (int[] collection)
+﻿internal class Program
 {
-    int lenth = collection.Length;
-    int index = 0;
-    while (index < lenth)
+    private static void Main(string[] args)
     {
-        collection[index] = int.Parse(Console.ReadLine());
-        index ++;
+        Console.Clear();
+
+        void FillArray(int[] collection)
+        {
+            int lenth = collection.Length;
+            int index = 0;
+            while (index < lenth)
+            {
+                collection[index] = int.Parse(Console.ReadLine()!);
+                index++;
+            }
+        }
+
+        Console.WriteLine("Выберете размерность. Введите 2 - 2D система  или 3 - 3D система");
+        int n = int.Parse(Console.ReadLine()!);
+        if (n == 3 || n == 2)
+        {
+            int[] array1 = new int[n];
+            int[] array2 = new int[n];
+
+            Console.WriteLine($"Вы выбрали {n}-ю значную систему исчесления");
+
+            FillArray(array1);
+            FillArray(array2);
+
+            Console.WriteLine();
+
+
+            int index = 0;
+            double distance = 0;
+
+            while (index < n)
+            {
+                distance = distance + (array2[index] - array1[index]) * (array2[index] - array1[index]);
+                index++;
+            }
+            Console.WriteLine(Math.Sqrt(distance));
+        }
+
+        else Console.WriteLine("Ошибка при выборе системы");
     }
 }
+
+        /*
+        Console.WriteLine("Введите координату х первой точки: ");
+        int x1 = int.Parse( Console.ReadLine());
+        Console.WriteLine("Введите координату у первой точки: ");
+        int y1 = int.Parse( Console.ReadLine());
+        Console.WriteLine("Введите координату z первой точки: ");
+        int z1 = int.Parse( Console.ReadLine());
+        Console.WriteLine("Введите координату x второй точки: ");
+        int x2 = int.Parse( Console.ReadLine());
+        Console.WriteLine("Введите координату y второй точки: ");
+        int y2 = int.Parse( Console.ReadLine());
+        Console.WriteLine("Введите координату z второй точки: ");
+        int z2 = int.Parse( Console.ReadLine());
+
+        double distance = Math.Sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1) + (z2-z1) * (z2-z1));
+
+        Console.WriteLine("Расстояние между точками в 3D пространстве = " + distance); */
