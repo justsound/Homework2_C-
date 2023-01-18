f303301ebe95aa51dc28dfe9c9cc37efcd1bdef9
Just_sound <just_sound@mail.ru>
2023-01-18 10:50:28 +0300

    Добавил метод ввода массива

--- Hw2_task4/Program.cs
+++ Hw2_task4/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Clear();
-
+/*
 Console.WriteLine("Введите координату х первой точки: ");
 int x1 = int.Parse( Console.ReadLine());
 Console.WriteLine("Введите координату у первой точки: ");
@@ -15,4 +15,15 @@ int z2 = int.Parse( Console.ReadLine());
 
 double distance = Math.Sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1) + (z2-z1) * (z2-z1));
 
-Console.WriteLine("Расстояние между точками = " + distance);
+Console.WriteLine("Расстояние между точками в 3D пространстве = " + distance); */
+
+void FillArray (int[] collection)
+{
+    int lenth = collection.Length;
+    int index = 0;
+    while (index < lenth)
+    {
+        collection[index] = int.Parse(Console.ReadLine());
+        index ++;
+    }
+}
