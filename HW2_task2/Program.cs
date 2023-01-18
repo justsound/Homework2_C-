943ab8d3a7f7818127c2fede79a2b883e3327a90
Just_sound <just_sound@mail.ru>
2023-01-18 10:04:47 +0300

    Добавил решение на определение квадратов

---
+++ HW2_task2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите первое целое число ");
+int number1 = int.Parse( Console.ReadLine());
+Console.WriteLine("Введите второе целое число ");
+int number2 = int.Parse( Console.ReadLine());
+int square = number2 * number2;
+if (number1 == number2*number2) Console.WriteLine("Является");
+else if (number2 == number1*number1) Console.WriteLine("Является");
+else Console.WriteLine("Не является");
