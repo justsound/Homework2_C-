fa77562a0007a93810a73928bc5c0c12985c42ae
Just_sound <just_sound@mail.ru>
2023-01-18 10:18:34 +0300

    Добавил решение нахождения расстояния по координатам

---
+++ Hw2_task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите координату х первой точки: ");
+int x1 = int.Parse( Console.ReadLine());
+Console.WriteLine("Введите координату у первой точки: ");
+int y1 = int.Parse( Console.ReadLine());
+Console.WriteLine("Введите координату z первой точки: ");
+int z1 = int.Parse( Console.ReadLine());
+Console.WriteLine("Введите координату x второй точки: ");
+int x2 = int.Parse( Console.ReadLine());
+Console.WriteLine("Введите координату y второй точки: ");
+int y2 = int.Parse( Console.ReadLine());
+Console.WriteLine("Введите координату z второй точки: ");
+int z2 = int.Parse( Console.ReadLine());
+
+double distance = Math.Sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1) + (z2-z1) * (z2-z1));
+
+Console.WriteLine("Расстояние между точками = " + distance);
